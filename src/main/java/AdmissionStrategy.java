62d5868c9c64de172d7e8d8e184505b45d6fc3e5
marones2 <marones2@illinois.edu>
2019-04-21 11:50:00 -0700

    added comments to AdmissionStrategy

--- src/main/java/AdmissionStrategy.java
+++ src/main/java/AdmissionStrategy.java
@@ -6,7 +6,12 @@ public interface AdmissionStrategy{
      * 
      * Every strategy class which implements this interface will have to 
      * implement the following methods, however there will be other methods 
-     * in each strategy class implementing the accept/reject criteria
+     * in each strategy class implementing the accept/reject criteria.
+     * 
+     * Did not add the rest of the methods implemented in StrategyOne because based 
+     * on the future accept/reject criteria they might not be needed at all, so no 
+     * reason to force the programmer to implement them.
+     * 
      */
     
     public String decision();
